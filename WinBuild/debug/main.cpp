d7cf6cd45698659b7f15a46f8757d87b976f7f59
shad7ows <sha7dows@gmail.com>
2024-09-15 17:28:56 +0300

    لا شيء مهم

--- WinBuild/debug/main.cpp
+++ WinBuild/debug/main.cpp
@@ -7,15 +7,15 @@ int main() {
 
 	auto start = high_resolution_clock::now();
 	for (auto i = 0; i < 50000000; i++) {
-		a = new int();
-		delete a;
+		a = (int*)malloc(4);
+		free(a);
 	}
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
 
 	std::cout << duration.count() / 1000 << " ms" << std::endl;
 
-	a = new int();
+	a = (int*)malloc(4);
 	*a = 99999;
 	std::cout << *a << std::endl;
 
